3a322eec8b03ad073a3ca241cace704c4da2b811
bharathboienipalli <bharathboienipalli@gmail.com>
2025-05-16 13:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+function divide(arr, n) {
+    const result = [];
+    let current = [];
+    let currentSum = 0;
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+    for (let i = 0; i < arr.length; i++) {
+        if (currentSum + arr[i] <= n) {
+            current.push(arr[i]);
+            currentSum += arr[i];
+        } else {
+            if (current.length > 0) {
+                result.push(current);
+            }
+            
+            current = [arr[i]];
+            currentSum = arr[i];
+        }
+    }
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+    
+    if (current.length > 0) {
+        result.push(current);
+    }
+
+    return result;
+}
